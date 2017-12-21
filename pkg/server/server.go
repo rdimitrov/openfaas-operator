1beeee9c60b7797568bd04dc40d37f9e84f2b648
Stefan Prodan <stefan.prodan@gmail.com>
2017-12-21 13:30:00 +0200

    Added Prometheus metrics route and pprof debug routes
    Signed-off-by: Stefan Prodan <stefan.prodan@gmail.com>

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"net/http"
+	_ "net/http/pprof"
 	"os"
 	"strconv"
 	"time"
@@ -9,6 +11,7 @@ import (
 	clientset "github.com/openfaas-incubator/faas-o6s/pkg/client/clientset/versioned"
 	"github.com/openfaas/faas-provider"
 	"github.com/openfaas/faas-provider/types"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func Start(client clientset.Interface) {
@@ -41,6 +44,11 @@ func Start(client clientset.Interface) {
 		}
 	}
 
+	pprof := "true"
+	if val, exists := os.LookupEnv("pprof"); exists {
+		pprof = val
+	}
+
 	bootstrapHandlers := types.FaaSHandlers{
 		FunctionProxy:  makeProxy(functionNamespace),
 		DeleteHandler:  makeDeleteHandler(functionNamespace, client),
@@ -58,6 +66,11 @@ func Start(client clientset.Interface) {
 		TCPPort:      &port,
 		EnableHealth: true,
 	}
+
+	if pprof == "true" {
+		bootstrap.Router().PathPrefix("/debug/pprof/").Handler(http.DefaultServeMux)
+	}
+	bootstrap.Router().Path("/metrics").Handler(promhttp.Handler())
 
 	glog.Infof("Using namespace %v", functionNamespace)
 	glog.Infof("Starting HTTP server on port %v", port)
